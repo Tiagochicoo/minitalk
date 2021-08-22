19d71ce0b28bcef35615b9d73fcb323da5e984f7
tpereira <tpereira@student.42lisboa.com>
2021-08-22 17:36:39 +0100

    server getting messages now

--- srcs/client.c
+++ srcs/client.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tpereira <tpereira@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/11 11:17:20 by tpereira          #+#    #+#             */
-/*   Updated: 2021/08/19 21:50:27 by tpereira         ###   ########.fr       */
+/*   Updated: 2021/08/22 17:36:15 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,14 @@ void	error(char *str)
 {
 	if (str)
 		free(str);
-	ft_putstr_fd("Unexpected error", 1);
+	ft_putstr_fd("Unexpected error.\n", 2);
 	exit(EXIT_FAILURE);
 }
 
 int	send_null(int pid, char *str)
 {
-	static int	i;
+	static int	i = 0;
 
-	i = 0;
 	if (i++ != 8)
 	{
 		if (kill(pid, SIGUSR1) == -1)
@@ -34,13 +33,11 @@ int	send_null(int pid, char *str)
 	return (1);
 }
 
-int	send_sig(char *str, int pid)
+int	send_string(int pid, char *str)
 {
-	static char	*string;
-	static int	bits;
+	static char	*string = 0;
+	static int	bits = -1;
 
-	string = 0;
-	bits = -1;
 	if (str)
 		string = ft_strdup(str);
 	if (!string)
@@ -58,40 +55,39 @@ int	send_sig(char *str, int pid)
 	}
 	if (!send_null(pid, string))
 		return (0);
-	free (string);
+	free(string);
 	return (1);
 }
 
-void	handle_sigusr(int sig)
+void	handler_sigusr(int signum)
 {
 	int	end;
 
 	end = 0;
-	if (sig == SIGUSR1)
-		end = send_sig(0, 0);
-	else if (sig == SIGUSR2)
+	if (signum == SIGUSR1)
+		end = send_string(0, 0);
+	else if (signum == SIGUSR2)
 	{
-		ft_putstr_fd("Unexpected error", 2);
+		ft_putstr_fd("Unexpected server error.\n", 2);
 		exit(EXIT_FAILURE);
 	}
 	if (end)
 	{
-		ft_putstr_fd("Message sent", 1);
+		ft_putstr_fd("Message delivered to server.\n", 1);
 		exit(EXIT_SUCCESS);
 	}
 }
 
 int	main(int argc, char **argv)
 {
-	if (argc != 3 || ft_isnumber(argv[1]) == 0)
+	if (argc != 3 || !ft_isnumber(argv[1]))
 	{
-		ft_putstr_fd("usage: ./client [server-pid] [message]\n", 1);
+		ft_putstr_fd("usage: ./client [server-pid] [message]\n", 2);
 		exit(EXIT_FAILURE);
 	}
-	// if you get a signal to the server the server sends a signal back and you should present a message in the client terminal
-	signal(SIGUSR1, handle_sigusr);
-	signal(SIGUSR2, handle_sigusr);
-	// send signal to server
-	send_sig(argv[2], ft_atoi(argv[1]));
-	return (0);
+	signal(SIGUSR1, handler_sigusr);
+	signal(SIGUSR2, handler_sigusr);
+	send_string(ft_atoi(argv[1]), argv[2]);
+	while (1)
+		pause();
 }
--- srcs/server.c
+++ srcs/server.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tpereira <tpereira@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/11 11:17:17 by tpereira          #+#    #+#             */
-/*   Updated: 2021/08/19 21:50:11 by tpereira         ###   ########.fr       */
+/*   Updated: 2021/08/22 17:36:16 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,25 @@ void	error(int pid, char *str)
 {
 	if (str)
 		free(str);
-	ft_putstr_fd("Unexpected error",2);
+	ft_putstr_fd("Unexpected error.\n", 2);
 	kill(pid, SIGUSR2);
 	exit(EXIT_FAILURE);
 }
 
-char*	print(char *str)
+char	*print_str(char *string)
 {
-	ft_putstr_fd(str, 1);
-	free(str);
+	string = ft_addchartostr(string, '\n');
+	ft_putstr_fd(string, 1);
+	free(string);
 	return (NULL);
 }
 
 void	handle_sigusr(int sig, siginfo_t *info, void *context)
 {
-	static char	c;
-	static int	bits;
-	static char	*str;
+	static char	c = 0xFF;
+	static int	bits = 0;
+	static char	*string = 0;
 
-	c = 0xFF;
-	bits = 0;
-	*str = 0;
 	(void)context;
 	if (sig == SIGUSR1)
 		c ^= 0x80 >> bits;
@@ -45,30 +43,30 @@ void	handle_sigusr(int sig, siginfo_t *info, void *context)
 	if (++bits == 8)
 	{
 		if (c)
-			str = ft_addchartostr(str, c);
+			string = ft_addchartostr(string, c);
 		else
-			str = print(str);
+			string = print_str(string);
 		bits = 0;
 		c = 0xFF;
 	}
 	if (kill(info->si_pid, SIGUSR1) == -1)
-		error(info->si_pid, str);
+		error(info->si_pid, string);
 }
 
 int	main(void)
 {
-	struct sigaction	sa_signal;
+	struct sigaction	sa;
 	sigset_t			block_mask;
 
 	sigemptyset(&block_mask);
 	sigaddset(&block_mask, SIGINT);
 	sigaddset(&block_mask, SIGQUIT);
-	sa_signal.sa_handler = 0;
-	sa_signal.sa_flags = SA_SIGINFO;
-	sa_signal.sa_mask = block_mask;
-	sa_signal.sa_sigaction = handle_sigusr;
-	sigaction(SIGUSR1, &sa_signal, NULL);
-	sigaction(SIGUSR2, &sa_signal, NULL);
+	sa.sa_handler = 0;
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_mask = block_mask;
+	sa.sa_sigaction = handle_sigusr;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	ft_putstr_fd("PID: ", 1);
 	ft_putnbr_fd(getpid(), 1);
 	ft_putstr_fd("\n", 1);
